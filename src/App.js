cec2e9801646e49e9493e52cfdbb102638e7c8a7
demaweiliya <qiwei@supermap.com>
2018-09-27 18:07:19 +0800

    lalala

--- src/App.js
+++ src/App.js
@@ -7,38 +7,11 @@ import 'react-day-picker/lib/style.css';
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-
-    this.state = {
-      selectedDay: new Date('2017', '3', '12'),
-      list:['weiwei','haha','duang'],
-      filterItem:"haha"
-    }
-    this.handleDayClick = this.handleDayClick.bind(this);
-  }
-
-  handleDayClick(day, { selected }) {
-    this.setState({
-      selectedDay:day,
-      filterItem:'weiwei'
-      //selectedDay: selected ? undefined : day,
-    });
-  }
-
   render() {
     return (
       <Provider store={store}>
       <Fragment>
         <Header/>
-        <DayPicker initialMonth={new Date(2017, 3)} selectedDays={this.state.selectedDay} onDayClick={this.handleDayClick}/>
-        {
-          this.state.list.map((item)=>{
-            if(item != this.state.filterItem){
-              return <li>{item}</li>
-            }
-          })
-        }
         </Fragment>
       </Provider>
     );
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React from 'react';
 import { CSSTransition } from 'react-transition-group'
 import { connect } from 'react-redux'
 import { HeaderWraper, 
@@ -11,41 +11,38 @@ import { HeaderWraper,
         SearchWraper
     } from './style'
 
-
-class Header extends Component {
-   
-    render(){
-        return (
-            <HeaderWraper>
-                <Logo/>
-                <Nav>
-                    <NavItem className="left active">首页</NavItem>
-                    <NavItem className="left">下载App</NavItem>
-                    <NavItem className="right">登陆</NavItem>
-                    <NavItem className="right">
-                        <i className="iconfont">&#xe636;</i>
-                    </NavItem>
-                    <SearchWraper>
-                        <CSSTransition timeout={200} in={this.props.focused} classNames="slide">
-                            <NavSearch className={this.props.focused ? 'focused' : ''}
-                                    onFocus={this.props.handleInputFocus}
-                                    onBlur={this.props.handleInputBlur}>
-                            </NavSearch>
-                        </CSSTransition>
-                        <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe6e4;</i>
-                    </SearchWraper>
-                    
-                </Nav>
-                <Addition>
-                    <Button className="write">
-                        <i className="iconfont">&#xe615;</i>
-                        写文章</Button>
-                    <Button className="reg">注册</Button>
-                </Addition>
-            </HeaderWraper>
-        )
-    }
+const Header = (props) => {
+    return (
+        <HeaderWraper>
+            <Logo/>
+            <Nav>
+                <NavItem className="left active">首页</NavItem>
+                <NavItem className="left">下载App</NavItem>
+                <NavItem className="right">登陆</NavItem>
+                <NavItem className="right">
+                    <i className="iconfont">&#xe636;</i>
+                </NavItem>
+                <SearchWraper>
+                    <CSSTransition timeout={200} in={props.focused} classNames="slide">
+                        <NavSearch className={props.focused ? 'focused' : ''}
+                                onFocus={props.handleInputFocus}
+                                onBlur={props.handleInputBlur}>
+                        </NavSearch>
+                    </CSSTransition>
+                    <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe6e4;</i>
+                </SearchWraper>
+                
+            </Nav>
+            <Addition>
+                <Button className="write">
+                    <i className="iconfont">&#xe615;</i>
+                    写文章</Button>
+                <Button className="reg">注册</Button>
+            </Addition>
+        </HeaderWraper>
+    )
 }
+
 
 const mapStateToProps = (state) => {
     return {
